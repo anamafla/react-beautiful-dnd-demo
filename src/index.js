c3b5a52cebc8c0c627f4052d86d17a095940d74a
Ana Mafla <anamafla@hotmail.com>
2019-10-11 09:36:25 -0500

    Persist List reordering

--- src/index.js
+++ src/index.js
@@ -9,12 +9,39 @@ function App() {
   const [columns, setColumns] = useState(initialData.columns);
   const [columnOrder, setColumnOrder] = useState(initialData.columnOrder);
 
-  console.log("tasks", tasks);
-  console.log("columns", columns);
-  console.log("columnOrder", columnOrder);
+  // console.log("tasks", tasks);
+  // console.log("columns", columns);
+  // console.log("columnOrder", columnOrder);
 
   const onDragEnd = result => {
-    // TODO: reorder our column
+    const { destination, source, draggableId } = result;
+
+    console.log("destination", destination);
+    console.log("source", source);
+    console.log("draggableId", draggableId);
+
+    if (!destination) {
+      return;
+    }
+
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    const column = columns[source.droppableId];
+    const newTaskIds = Array.from(column.taskIds);
+    newTaskIds.splice(source.index, 1);
+    newTaskIds.splice(destination.index, 0, draggableId);
+
+    const newColumn = {
+      ...column,
+      taskIds: newTaskIds
+    };
+
+    setColumns(prevState => ({ prevState, [newColumn.id]: newColumn }));
   };
 
   return (
